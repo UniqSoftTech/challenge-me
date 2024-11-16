f473c67589c7b2fbe4abaabb81fb7072f89fcd0d
Grey <grey@uniqsoft.tech>
2024-11-16 18:27:47 +0700

    updateable user info

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -12,4 +12,26 @@ export class UserController {
       res.status(500).json({ message: 'Internal server error', error });
     }
   };
+
+  put = async (req: any, res: Response) => {
+    try {
+      const user = await db.Users.findByPk(req.user.id);
+
+      if (!user)
+        return res.status(404).json({ message: 'User not found' });
+
+      user.proof = req.body.proof;
+      user.age = req.body.age;
+      user.name = req.body.name;
+      user.height = req.body.height;
+      user.weight = req.body.weight;
+      user.birthday = req.body.birthday;
+      user.relationship_status = req.body.relationship_status;
+
+      await user.save();
+      res.status(201).json({ message: 'User updated successfully', status: true, data: user });
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error', error });
+    }
+  };
 }
