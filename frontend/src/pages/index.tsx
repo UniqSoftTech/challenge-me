68518caed1fe0bd3dd7e111b78b95bf5e11ce26b
Jay <jay@uniqsoft.tech>
2024-11-17 00:11:12 +0700

    fornt end changes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -11,6 +11,9 @@ import { IDKitWidget, VerificationLevel } from "@worldcoin/idkit";
 import { useAuth } from "@/context/authContext";
 import Button from "@/components/data-display/Button";
 import { useRouter } from "next/router";
+import useGlobalRequestStore from "@/hooks/useGlobalRequestStore";
+import Image from "next/image";
+import Art from "../assets/art.svg";
 
 const privateKeyProvider = new EthereumPrivateKeyProvider({
   config: { chainConfig },
@@ -25,6 +28,8 @@ const web3auth = new Web3Auth(web3AuthOptions);
 
 function App() {
   const router = useRouter();
+  const { requests, execute } = useGlobalRequestStore();
+
   const { walletAddress, setWalletAddress } = useAuth();
   const [provider, setProvider] = useState<IProvider | null>(null);
 
@@ -53,12 +58,30 @@ function App() {
 
   const login = async () => {
     const web3authProvider = await web3auth.connect();
+    console.log("🚀 ~ login ~ web3authProvider:", web3authProvider);
     setProvider(web3authProvider);
-    if (web3auth.connected) {
-      getAccounts();
-    }
+    console.log("🚀 ~ login ~ web3auth:", web3auth);
 
-    router.push("/verification");
+    if (web3auth.connected) {
+      const address = await getAccounts();
+
+      await execute(
+        "login",
+        { method: "POST", url: "user/signin" },
+        {
+          data: { wallet: address },
+          onSuccess: async (result) => {
+            if (result.status === 200) {
+              await localStorage.setItem("token", result?.data?.data?.token);
+              router.push("/app");
+            } else {
+              router.push("/verification");
+            }
+          },
+          onError: (error) => router.push("/verification"),
+        },
+      );
+    }
   };
 
   const getAccounts = async () => {
@@ -66,7 +89,7 @@ function App() {
       return;
     }
     const address = await ethersRPC.getAccounts(provider);
-    setWalletAddress(address);
+    return address;
   };
 
   const logout = async () => {
@@ -90,19 +113,46 @@ function App() {
   );
 
   return (
-    <div className="flex flex-col">
-      <header className="text-center">
-        <h1 className="text-4xl font-extrabold mb-4 bg-gradient-to-r from-green-400 to-blue-500 text-transparent bg-clip-text">
-          Hello World
-        </h1>
-        <p className="text-lg text-gray-400">Web 3 auth</p>
-      </header>
-
-      <main className="flex flex-col items-center mt-8">
-        <div className="flex flex-col items-center justify-center gap-6">
-          {unloggedInView}
+    <div className="flex flex-col min-h-screen bg-[#00D084] text-black">
+      <div className="flex justify-between items-center px-4 py-2">
+        <div className="text-black text-sm font-bold">12:45</div>
+        <div className="flex gap-2 items-center">
+          <div className="w-2.5 h-2.5 bg-black rounded-full"></div>
+          <div className="w-2.5 h-2.5 bg-black rounded-full"></div>
+          <div className="w-2.5 h-2.5 bg-black rounded-full"></div>
         </div>
-      </main>
+      </div>
+
+      <div className="flex flex-col justify-center flex-grow relative gap-12">
+        <h1 className="text-4xl font-extrabold px-6 text-black">
+          Challenge
+          <span className="text-white">Me</span>
+        </h1>
+
+        {/* Image */}
+        <div className="top-32">
+          <Image
+            src={Art}
+            alt="Art"
+            width={300}
+            height={300}
+            className="z-10"
+          />
+        </div>
+
+        {/* Background shape */}
+        <div className="absolute top-20 -z-10 w-[300px] h-[300px] bg-[#FFC107] rounded-[50%] clip-path-custom" />
+      </div>
+
+      {/* Submit Button */}
+      <div className="flex justify-center px-4 pb-8">
+        <button
+          onClick={login}
+          className="px-8 w-full py-4 bg-black text-white text-xl font-semibold rounded-lg shadow-lg hover:bg-gray-800 transition"
+        >
+          Connect Your Wallet
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/verification.tsx
+++ frontend/src/pages/verification.tsx
@@ -1,49 +1,42 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useWeb3Auth } from "@web3auth/modal-react-hooks";
 import ethersRPC from "@/utils/ethersRPC";
+import useGlobalRequestStore from "@/hooks/useGlobalRequestStore";
+import { useRouter } from "next/router";
+import Button from "@/components/data-display/Button";
 
-// Update the FormDataKeys type to exclude `proof` and handle new requirements
 type FormDataKeys =
   | "name"
-  | "age"
+  | "birthday"
   | "height"
   | "weight"
-  | "birthday"
   | "relationship_status";
 
-// Update the FormData interface to exclude `proof`
 interface FormData {
   name: string;
-  age: string;
+  birthday: string;
   height: string;
   weight: string;
-  birthday: string;
   relationship_status: string;
 }
 
 const App = () => {
+  const router = useRouter();
   const [step, setStep] = useState(0);
-  const {
-    status,
-    connect,
-    addAndSwitchChain,
-    userInfo,
-    provider,
-    web3Auth,
-    authenticateUser,
-  } = useWeb3Auth();
+  const { provider } = useWeb3Auth();
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
+  const { execute } = useGlobalRequestStore();
 
   const [formData, setFormData] = useState<FormData>({
     name: "",
-    age: "",
     height: "",
     weight: "",
     birthday: "",
     relationship_status: "",
   });
+
   const [errors, setErrors] = useState<Partial<FormData>>({});
 
   useEffect(() => {
@@ -51,27 +44,50 @@ const App = () => {
   }, []);
 
   const getAccounts = async () => {
-    if (!provider) {
-      return;
-    }
+    if (!provider) return;
     const address = await ethersRPC.getAccounts(provider);
     setWalletAddress(address);
   };
 
-  const fields: {
-    name: FormDataKeys;
-    label: string;
-    type: string;
-    options?: string[];
-  }[] = [
-    { name: "name", label: "Name", type: "text" },
-    { name: "age", label: "Age", type: "number" },
-    { name: "height", label: "Height", type: "number" },
-    { name: "weight", label: "Weight", type: "number" },
-    { name: "birthday", label: "Birthday", type: "date" },
+  const fields = [
+    {
+      name: "name",
+      label: "How do we call you?",
+      title: "We need some information about you.",
+      type: "text",
+      placeholder: "Username...",
+    },
+    {
+      name: "birthday",
+      label: "When’s your birthday?",
+      type: "date",
+      title: "It’s nice to meet you.",
+    },
+    {
+      name: "gender",
+      label: "How do you identify yourself?",
+      title: "So you are a Scorpio.",
+      type: "select",
+      options: ["she/her", "he/him", "they/them", "other"],
+    },
+    {
+      name: "height",
+      label: "How tall do you stand?",
+      title: "We’re all about celebrating individuality here.",
+      type: "number",
+      placeholder: "CM",
+    },
+    {
+      name: "weight",
+      label: "What’s the number on the scale?",
+      title: "Perfect for reaching goals (and maybe high shelves).",
+      type: "number",
+      placeholder: "KG",
+    },
     {
       name: "relationship_status",
-      label: "Relationship Status",
+      label: "What’s your current love story?",
+      title: "Let’s make that number work for you.",
       type: "select",
       options: ["Single", "Married", "In a Relationship", "It's Complicated"],
     },
@@ -81,14 +97,13 @@ const App = () => {
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
     const { name, value } = e.target;
-
     setFormData((prev) => ({ ...prev, [name]: value }));
-    setErrors((prev) => ({ ...prev, [name]: "" })); // Clear error on input change
+    setErrors((prev) => ({ ...prev, [name]: "" }));
   };
 
   const handleNext = () => {
     const field = fields[step];
-    if (!formData[field.name]) {
+    if (!formData[field.name as FormDataKeys]) {
       setErrors((prev) => ({
         ...prev,
         [field.name]: `${field.label} is required`,
@@ -102,98 +117,123 @@ const App = () => {
     if (step > 0) setStep(step - 1);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const isFormComplete = Object.keys(formData).every(
-      (key) => formData[key as FormDataKeys],
+      (key) => formData[key as FormDataKeys]?.trim() !== "",
     );
+
+    console.log("🚀 ~ handleSubmit ~ isFormComplete:", isFormComplete);
+
     if (!isFormComplete) {
       const newErrors: Partial<FormData> = {};
       Object.keys(formData).forEach((key) => {
-        if (!formData[key as FormDataKeys]) {
+        if (!formData[key as FormDataKeys]?.trim()) {
           newErrors[key as FormDataKeys] = `${key} is required`;
         }
       });
       setErrors(newErrors);
       return;
     }
-    // alert("Form submitted successfully!");
-    console.log("Submitted Data:", formData);
-    console.log("User Info:", walletAddress);
+
+    await execute(
+      "user",
+      { method: "POST", url: "user" },
+      {
+        data: { ...formData, wallet: walletAddress },
+        onSuccess: (data) => console.log("data", data),
+        onError: (error) => console.error("error", error),
+      },
+    );
   };
 
   const currentField = fields[step];
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-6 bg-white rounded shadow-lg w-96">
-        <h1 className="text-2xl font-semibold mb-4">
-          Step {step + 1} of {fields.length}
-        </h1>
+    <div className="flex flex-col min-h-screen bg-white gap-3">
+      {/* Header */}
+      <div className="flex flex-col pt-4 px-4">
         <div className="mb-4">
-          <label
-            htmlFor={currentField.name}
-            className="block text-gray-700 font-medium"
-          >
-            {currentField.label}
-          </label>
-          {currentField.type === "select" ? (
-            <select
-              id={currentField.name}
-              name={currentField.name}
-              value={formData[currentField.name]}
-              onChange={handleInputChange}
-              className={`w-full px-4 py-2 border ${
-                errors[currentField.name] ? "border-red-500" : "border-gray-300"
-              } rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
-            >
-              <option value="">Select {currentField.label}</option>
-              {currentField.options?.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          ) : (
-            <input
-              id={currentField.name}
-              name={currentField.name}
-              type={currentField.type}
-              value={formData[currentField.name]}
-              onChange={handleInputChange}
-              className={`w-full px-4 py-2 border ${
-                errors[currentField.name] ? "border-red-500" : "border-gray-300"
-              } rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
-            />
-          )}
-          {errors[currentField.name] && (
-            <p className="text-red-500 text-sm mt-1">
-              {errors[currentField.name]}
-            </p>
-          )}
-        </div>
-        <div className="flex justify-between">
           <button
             onClick={handlePrevious}
-            disabled={step === 0}
-            className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 disabled:bg-gray-200 disabled:cursor-not-allowed"
+            className={`text-lg ${step === 0 ? "invisible" : ""}`}
           >
-            Previous
+            ←
           </button>
-          {step < fields.length - 1 ? (
-            <button
-              onClick={handleNext}
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-              Continue
-            </button>
-          ) : (
-            <button
-              onClick={handleSubmit}
-              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
-            >
-              Submit
-            </button>
-          )}
+        </div>
+        <div className="text-black text-xl">{fields[step].title}</div>
+      </div>
+
+      {/* Progress Bar */}
+      <div className="w-full px-4">
+        <div className="relative h-1 bg-gray-300 rounded-full">
+          <div
+            className="absolute h-1 bg-black rounded-full transition-all"
+            style={{ width: `${((step + 1) / fields.length) * 100}%` }}
+          />
+        </div>
+      </div>
+
+      {/* Content */}
+      <div className="flex-grow flex flex-col justify-center px-6 gap-1">
+        <h1 className="text-base mb-4">{currentField.label}</h1>
+
+        {currentField.type === "select" ? (
+          <select
+            id={currentField.name}
+            name={currentField.name}
+            value={formData[currentField.name as FormDataKeys]}
+            onChange={handleInputChange}
+            className={`w-full px-4 py-3 font-bold text-xl rounded-2xl border-2 ${
+              errors[currentField.name as FormDataKeys]
+                ? "border-red-500"
+                : formData[currentField.name as FormDataKeys]
+                ? "border-black"
+                : "border-gray-300"
+            } rounded focus:outline-none focus:ring-1 focus:border-black focus:ring-black`}
+          >
+            <option value="">Select...</option>
+            {currentField.options?.map((option) => (
+              <option key={option} className="font-bold text-xl" value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            id={currentField.name}
+            name={currentField.name}
+            type={currentField.type}
+            placeholder={currentField.placeholder}
+            value={formData[currentField.name as FormDataKeys]}
+            onChange={handleInputChange}
+            className={`w-full font-bold text-xl px-4 py-3 rounded-2xl border-2 ${
+              errors[currentField.name as FormDataKeys]
+                ? "border-red-500"
+                : formData[currentField.name as FormDataKeys]
+                ? "border-black"
+                : "border-gray-300"
+            } rounded focus:outline-none focus:ring-1 focus:border-black focus:ring-black`}
+          />
+        )}
+        {errors[currentField.name as FormDataKeys] && (
+          <p className="text-red-500 text-sm mt-1">
+            {errors[currentField.name as FormDataKeys]}
+          </p>
+        )}
+        <div className="flex justify-end mt-4">
+          <div className="w-1/2 flex-end">
+            {step < fields.length - 1 ? (
+              <Button
+                title="Next"
+                onPress={handleNext}
+                disabled={
+                  errors[currentField.name as FormDataKeys] ? true : false
+                }
+              />
+            ) : (
+              <Button title="Submit" onPress={handleSubmit} />
+            )}
+          </div>
         </div>
       </div>
     </div>
