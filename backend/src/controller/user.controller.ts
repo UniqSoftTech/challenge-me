50be3d3d4e4fc86ff9d0182e8bb2e43cf611b9c4
Grey <grey@uniqsoft.tech>
2024-11-16 21:57:50 +0700

    bug fix

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -8,7 +8,7 @@ export class UserController {
       const { proof, age, name, height, weight, birthday, relationship_status, wallet } = req.body;
 
       const user = await db.Users.create({ proof, age, name, height, weight, birthday, relationship_status, wallet });
-      const token = generateToken(user);
+      const token = generateToken(user.toJSON());
       res.status(201).json({ message: 'User created successfully', status: true, data: { user, token } });
     } catch (error) {
       res.status(500).json({ message: 'Internal server error', error });
@@ -23,10 +23,10 @@ export class UserController {
       if (!user)
         return res.status(404).json({ message: 'User not found', status: false });
 
-      const token = generateToken(user);
+      const token = generateToken(user.toJSON());
       res.status(200).json({ status: true, data: { user, token } });
     } catch (error) {
-      res.status(500).json({ message: 'Internal server error', error });
+      res.status(500).json({ message: 'Internal server error', error: JSON.stringify(error) });
     }
   }
 
