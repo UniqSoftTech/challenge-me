582b17d09183cf6111918b1a6f0f1c045191f06c
Grey <grey@uniqsoft.tech>
2024-11-16 18:00:58 +0700

    added middleware and jwt token generate

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,13 +1,15 @@
 import db from "../models/_index";
 import { Request, Response } from 'express';
+import { generateToken } from "../utils/jwt.utils";
 
 export class UserController {
   create = async (req: Request, res: Response) => {
     try {
-      await db.Users.create(req.body);
-      res.status(201).json({ message: 'User created successfully' });
+      const user = await db.Users.create(req.body);
+      const token = generateToken(user);
+      res.status(201).json({ message: 'User created successfully', status: true, data: { user, token } });
     } catch (error) {
       res.status(500).json({ message: 'Internal server error', error });
     }
-  }
+  };
 }
